8edb373d6aefe1a4284e35e0e0336ff30eb5c28a
Darren Wilkinson <darrenjwilkinson@btinternet.com>
2023-06-17 22:21:00 +0100

    sqrt example

---
+++ Scala/sqrt.scala
@@ -0,0 +1,30 @@
+//> using scala 3.3.0
+
+/*
+sqrt.scala
+Compute square roots with Newton's method
+
+Run with:
+scala-cli sqrt.scala -- 2.0
+
+*/
+
+
+object SqrtApp:
+
+  @main
+  def sqrtMain(x: Double) =
+    print(s"$x = ")
+    println(mySqrt(x))
+
+  def mySqrt(x: Double): Double =
+    @annotation.tailrec
+    def go(sx: Double): Double =
+      if (math.abs(sx*sx - x) < 1.0e-8)
+        sx
+      else
+        go((sx + x/sx)/2)
+    go(x)
+
+// eof
+
